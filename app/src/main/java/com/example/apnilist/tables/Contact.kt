b6260c30b7310dbeb12e7664bea03522d3096253
naveenkanojiya <naveenkanojiya9956@gmail.com>
2023-12-16 23:57:58 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/apnilist/tables/Contact.kt
@@ -0,0 +1,17 @@
+package com.example.apnilist.tables
+
+import androidx.room.Entity
+import java.util.Date
+
+@Entity(tableName = "contact_table")
+data class Contact (
+    var id:Int,
+
+    var name:String,
+
+    var number: String,
+
+    var email : String,
+
+    var dob: Date
+)
